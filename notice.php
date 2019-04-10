981be8f9413f7c1753c706e241298db863e6574c
Mr.Hope <zhangbowang1998@gmail.com>
2019-04-10 21:01:49 +0800

    php格式化

--- notice.php
+++ notice.php
@@ -1,37 +1,44 @@
 <?php
 /**
- * @Author: Mr.Hope
- * @LastEditors: Mr.Hope
- * @Description: 通知处理
- * @Date: 2019-03-11 23:12:14
- * @LastEditTime: 2019-03-18 17:56:04
+ * Notice Handler
+ *
+ * PHP version 7
+ *
+ * @category  Notice
+ * @package   Notice
+ * @author    Mr.Hope <zhangbowang1998@gmail.com>
+ * @copyright 2019 HopeStudio
+ * @license   No License
+ * @link      http://nenuyouth.com
  */
 
-declare (strict_types = 1);
+declare(strict_types = 1);
 
 header("Content-Type: text/html; charset=utf-8");
+
 /**
- * @description: 获取指定文件并转为字符串
+ * 获取指定文件并转为字符串
  *
- * @param $url {string} url地址
+ * @param string $url url地址
  *
- * @return: 获取到的字符串数据
+ * @return string 获取到的字符串数据
  */
 function httpget(string $url): string
 {
-  $curl = curl_init();
-  curl_setopt($curl, CURLOPT_URL, $url);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-  $response = curl_exec($curl);
-  curl_close($curl);
-  return $response;
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
 }
+
 // 从地址中获得url
 $url = $_GET["url"];
 // 从url中获取id
 $id = substr($url, -19, 10);
 // 移动到notice目录
-chdir("/www/wwwroot/mp/notice");
+chdir("notice");
 
 echo '开始匹配' . $id . "\n";
 
@@ -39,7 +46,11 @@ echo '开始匹配' . $id . "\n";
 $noticeHtml = httpget("http://mid.nenu.edu.cn:8080/xxhbwx/HttpReq?appid=2&url=$url");
 
 // 处理通知内容，去除br，无用换行和回车，以及html注释
-$noticeContent = preg_replace(["/<br\/>/", "/[\r\n\t]/", "/<!--.*?-->/"], ["#@", "", ""], $noticeHtml);
+$noticeContent = preg_replace(
+    ["/<br\/>/", "/[\r\n\t]/", "/<!--.*?-->/"],
+    ["#@", "", ""],
+    $noticeHtml
+);
 
 echo '通知内容是：' . $noticeContent;
 // notice匹配规则
@@ -48,43 +59,56 @@ $noticeRule = '/(.*)\${6}.*ent\">(.*)<p s.*?ht;\">(.*?)<\/p/';
 preg_match_all($noticeRule, $noticeContent, $noticeData);
 //输出通知信息
 var_dump($noticeData);
-$text = preg_replace(["/(<\/p><p.*?>)|(#@)/", "/<\/p>|<p(.*)?>/"], ["\n", ""], $noticeData[2][0]);
+$text = preg_replace(
+    ["/(<\/p><p.*?>)|(#@)/", "/<\/p>|<p(.*)?>/"],
+    ["\n", ""],
+    $noticeData[2][0]
+);
 $footer = preg_replace("/#@/", "\n", $noticeData[3][0]);
 preg_match_all('/附件：(?:[0-9]．)?/', $text, $temp, PREG_OFFSET_CAPTURE);
 var_dump($temp);
 if ($temp[0]) {
-  $position = $temp[0][0][1];
-  var_dump($position);
-  echo "找到附件！<br />";
-  $attachmentString = preg_replace("/\n/", "", substr($text, $position + 9));
-  $mutiAttachmentRule = "/(?:[0-9]．)?.*?f=\"(.*?)\".*?\">(.*?)\.(.*?)<\/a>/";
-  preg_match_all($mutiAttachmentRule, $attachmentString, $attachmentData);
-  var_dump($attachmentData);
-  $attachment = array();
-  echo "附件匹配成功！<br />";
-  $dir = iconv("UTF-8", "GBK", $id);
-  if (!file_exists($dir)) {
-    mkdir($dir, 0777, true);
-    echo "创建文件夹" . $id . "成功。<br />";
-  } else {
-    echo "文件夹" . $id . "已存在。<br />";
-  }
-  for ($j = 0; $j < count($attachmentData[0]); $j++) {
-    $attchUrl = 'http://www.nenu.edu.cn' . $attachmentData[1][$j];
-    $attchFile = file_get_contents($attchUrl);
-    $attchName = pathinfo($attchUrl)["basename"];
-    file_put_contents("$id/" . $attchName, $attchFile);
-    $attachment[$j] = array(
-      'tag' => 'doc',
-      'url' => 'https://mrhope.top/mpServer/notice/' . $id . '/' . $attchName,
-      'docName' => $attachmentData[2][$j] . '.' . $attachmentData[3][$j]
+    $position = $temp[0][0][1];
+    var_dump($position);
+    echo "找到附件！<br />";
+    $attachmentString = preg_replace("/\n/", "", substr($text, $position + 9));
+    $mutiAttachmentRule = "/(?:[0-9]．)?.*?f=\"(.*?)\".*?\">(.*?)\.(.*?)<\/a>/";
+    preg_match_all($mutiAttachmentRule, $attachmentString, $attachmentData);
+    var_dump($attachmentData);
+    $attachment = array();
+    echo "附件匹配成功！<br />";
+    $dir = iconv("UTF-8", "GBK", $id);
+    if (!file_exists($dir)) {
+        mkdir($dir, 0777, true);
+        echo "创建文件夹" . $id . "成功。<br />";
+    } else {
+        echo "文件夹" . $id . "已存在。<br />";
+    }
+    for ($j = 0; $j < count($attachmentData[0]); $j++) {
+        $attchUrl = 'http://www.nenu.edu.cn' . $attachmentData[1][$j];
+        $attchFile = file_get_contents($attchUrl);
+        $attchName = pathinfo($attchUrl)["basename"];
+        file_put_contents("$id/" . $attchName, $attchFile);
+        $attachment[$j] = array(
+          'tag' => 'doc',
+          'url' => 'https://mrhope.top/mpServer/notice/' . $id . '/' . $attchName,
+          'docName' => $attachmentData[2][$j] . '.' . $attachmentData[3][$j]
+        );
+    }
+    $text = substr($text, 0, $position);
+    $notice = array(
+      "title" => $noticeData[1][0],
+      "content" => $text,
+      "attachment" => $attachment,
+      "footer" => $footer
     );
-  }
-  $text = substr($text, 0, $position);
-  $notice = array("title" => $noticeData[1][0], "content" => $text, "attachment" => $attachment, "footer" => $footer);
 } else {
-  echo '没有附件！<br />';
-  $notice = array("title" => $noticeData[1][0], "content" => $text, "footer" => $footer);
+    echo '没有附件！<br />';
+    $notice = array(
+      "title" => $noticeData[1][0],
+      "content" => $text,
+      "footer" => $footer
+    );
 }
 echo $text;
 $tableRule = '/(?:\<div.*?\>)?\<table.*?\>(?=.|\n)*?\<\/table>(?:\<\/div>)?/';
@@ -92,21 +116,21 @@ $tableRule = '/(?:\<div.*?\>)?\<table.*?\>(?=.|\n)*?\<\/table>(?:\<\/div>)?/';
 preg_match_all($tableRule, $text, $tableData);
 var_dump($tableData);
 for ($l = 0; $l < count($tableData[0]); $l++) {
-  $tableStartPosition = strpos($text, $tableData[0][$l]);
-  $tableEndPosition = $tableStartPosition + strlen($tableData[0][$l]);
-  $tableString = preg_replace("/\n/", "", $tableData[0][$l]);
-  $tableRule2 = '/<table.*?\><tbody>(.*?)<\/tbody><\/table>/';
-  preg_match_all($tableRule2, $tableString, $tableData2);
-  var_dump($tableData2);
-  $tableString2 = $tableData2[1][0];
-  $tableRule3 = '/<tr.*?\>(.*?)<\/tr>/';
-  preg_match_all($tableRule3, $tableString2, $tableData3);
-  var_dump($tableData3);
+    $tableStartPosition = strpos($text, $tableData[0][$l]);
+    $tableEndPosition = $tableStartPosition + strlen($tableData[0][$l]);
+    $tableString = preg_replace("/\n/", "", $tableData[0][$l]);
+    $tableRule2 = '/<table.*?\><tbody>(.*?)<\/tbody><\/table>/';
+    preg_match_all($tableRule2, $tableString, $tableData2);
+    var_dump($tableData2);
+    $tableString2 = $tableData2[1][0];
+    $tableRule3 = '/<tr.*?\>(.*?)<\/tr>/';
+    preg_match_all($tableRule3, $tableString2, $tableData3);
+    var_dump($tableData3);
 }
 var_dump($notice);
 $noticeString = json_encode($notice, 320);
 $noticeResult = file_put_contents("$id.json", $noticeString);
 
 if ($noticeResult) {
-  echo '通知' . $id . "生成成功!<br />";
+    echo '通知' . $id . "生成成功!<br />";
 }
--- noticeList.php
+++ noticeList.php
@@ -1,7 +1,31 @@
 <?php
+/**
+ * NoticeList Handler
+ *
+ * PHP version 7
+ *
+ * @category  Notice
+ * @package   Notice
+ * @author    Mr.Hope <zhangbowang1998@gmail.com>
+ * @copyright 2019 HopeStudio
+ * @license   No License
+ * @link      http://nenuyouth.com
+ */
+
 header("Content-Type: text/html; charset=utf-8");
-$forceUpdate=isset($_GET['force'])?true:false;
-function http_get($url)
+
+// 是否强行升级
+$forceUpdate = isset($_GET['force']) ? true : false;
+
+
+/**
+ * 网站数据请求函数
+ *
+ * @param string $url 网页地址
+ *
+ * @return string 网页的string内容
+ */
+function Http_get($url)
 {
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
@@ -10,36 +34,82 @@ function http_get($url)
     curl_close($curl);
     return $response;
 }
-chdir("/www/wwwroot/mp/notice");
-$listContent = preg_replace('/[\n\r\t]*/', '', http_get("http://mid.nenu.edu.cn:8080/xxhbwx/HttpReq?url=www.nenu.edu.cn/_s4/tzggwmhzqw/list1.psp&appid=2"));
+
+// 创建通知文件夹
+chdir("notice");
+
+// 将从内网公告网址得到的字符串做处理，去掉其中的制表、换行符号
+$listContent = preg_replace(
+    '/[\n\r\t]*/',
+    '',
+    Http_get("http://mid.nenu.edu.cn:8080/xxhbwx/HttpReq?url=www.nenu.edu.cn/_s4/tzggwmhzqw/list1.psp&appid=2")
+);
+
+// 设置通知列表匹配规则
 $listRule='/\',\'(.*?)\'.*?x;\'>(.*?)<\/d.*?\'>(.*?)</';
+
+// 将匹配到的结果写入到listData数组中
 preg_match_all($listRule, $listContent, $listData);
+
+// 新建noticeList数组
 $noticeList=array();
+
+// 输入代码标签
 echo "<pre>";
-for ($i=0;$i<count($listData[0]);$i++) {
+
+// 对listData的每一项进行处理
+for ($i = 0; $i < count($listData[0]); $i++) {
+
+    // 获得通知的地址和id
     $url=ltrim($listData[1][$i], 'http://');
     $id=substr($url, -19, 10);
+
+    // 如果相应id的json文件不存在或者启用了强制更新
     if (!file_exists($id.'.json')||$forceUpdate==true) {
         echo '开始匹配'.$id."<br />";
-        $noticeHtml= http_get("http://mid.nenu.edu.cn:8080/xxhbwx/HttpReq?appid=2&url=$url");
-        $noticeContent = preg_replace(["/<br\/>/","/[\r\n\t]/","/<!--.*?-->/"], ["#@","",""], $noticeHtml);
+
+        // 获得通知内容，并对换行符进行处理，去掉空格制表符还有注释
+        $noticeHtml= Http_get("http://mid.nenu.edu.cn:8080/xxhbwx/HttpReq?appid=2&url=$url");
+        $noticeContent = preg_replace(
+            ["/<br\/>/","/[\r\n\t]/","/<!--.*?-->/"],
+            ["#@","",""],
+            $noticeHtml
+        );
+
+        // 按照特定规则匹配通知内容
         $noticeRule='/(.*)\${6}.*ent\">(.*)<p s.*?ht;\">(.*?)<\/p/';
         preg_match_all($noticeRule, $noticeContent, $noticeData);
-        $text = preg_replace(["/(<\/p><p.*?>)|(#@)/","/<\/p>|<p(.*)?>/"], ["\n",""], $noticeData[2][0]);
+
+        // 得到正文和页脚
+        $text = preg_replace(
+            ["/(<\/p><p.*?>)|(#@)/","/<\/p>|<p(.*)?>/"],
+            ["\n",""],
+            $noticeData[2][0]
+        );
         $footer = preg_replace("/#@/", "\n", $noticeData[3][0]);
+
+        // 使用特定规则对正文进行匹配寻找附件
         preg_match_all('/附件：(?:[0-9]．)?/', $text, $temp, PREG_OFFSET_CAPTURE);
         var_dump($temp);
+
+        // 如果找到附件
         if ($temp[0]) {
             $position=$temp[0][0][1];
             var_dump($position);
             echo "找到附件！<br />";
+
+            // 获得附件字符串
             $attachmentString = preg_replace("/\n/", "", substr($text, $position+9));
             var_dump($attachmentString);
+
+            // 匹配出每一个附件
             $mutiAttachmentRule="/(?:[0-9]．)?.*?f=\"(.*?)\".*?\">(.*?)\.(.*?)<\/a>/";
             preg_match_all($mutiAttachmentRule, $attachmentString, $attachmentData);
             var_dump($attachmentData);
             $attachment=array();
             echo "附件匹配成功！<br />";
+
+            // 生成附件的存放文件夹
             $dir = iconv("UTF-8", "GBK", $id);
             if (!file_exists($dir)) {
                 mkdir($dir, 0777, true);
@@ -47,6 +117,8 @@ for ($i=0;$i<count($listData[0]);$i++) {
             } else {
                 echo "文件夹".$id."已存在。<br />";
             }
+
+            // 获取附件并写入至附件文件夹
             for ($j=0;$j<count($attachmentData[0]);$j++) {
                 $attchUrl='http://www.nenu.edu.cn'.$attachmentData[1][$j];
                 $attchFile=file_get_contents($attchUrl);
@@ -58,13 +130,30 @@ for ($i=0;$i<count($listData[0]);$i++) {
                     'docName'=>$attachmentData[2][$j].'.'.$attachmentData[3][$j]
                 );
             }
+
+            // 重新获得正文内容
             $text = substr($text, 0, $position);
-            $notice = array("title" => $noticeData[1][0], "content" => $text, "attachment" => $attachment, "footer" => $footer);
+
+            // 生成通知数据
+            $notice = array(
+              "title" => $noticeData[1][0],
+              "content" => $text,
+              "attachment" => $attachment,
+              "footer" => $footer
+            );
         } else {
             echo '没有附件！<br />';
-            $notice = array("title" => $noticeData[1][0], "content" => $text, "footer" => $footer);
+
+            // 生成通知数据
+            $notice = array(
+                "title" => $noticeData[1][0],
+                "content" => $text,
+                "footer" => $footer
+            );
         }
         var_dump($notice);
+
+        // 把通知序列化写入json
         $noticeString = json_encode($notice, 320);
         $noticeResult = file_put_contents("$id.json", $noticeString);
         if ($noticeResult) {
@@ -73,6 +162,8 @@ for ($i=0;$i<count($listData[0]);$i++) {
     } else {
         echo "已生成通知".$id."<br />";
     }
+
+    // 生成通知列表
     $noticeList[$i]=array(
         'id'=>$id,
         'time'=>$listData[3][$i],
